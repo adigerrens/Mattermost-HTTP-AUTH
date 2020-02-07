f91bb8fd152b8fcc4946152e6d784d289c5fce8e
Gawan Errenst <Gawan.Errenst@aspera.com>
2020-02-07 09:02:44 +0100

    allow mutliple base_dn

--- oauth/connexion.php
+++ oauth/connexion.php
@@ -5,73 +5,67 @@ session_start();
  */
 
 // include our LDAP object
-require_once __DIR__.'/LDAP/LDAP.php';
-require_once __DIR__.'/LDAP/config_ldap.php';
+require_once __DIR__ . '/LDAP/LDAP.php';
+require_once __DIR__ . '/LDAP/config_ldap.php';
 
 
 // Verify all fields have been filled
-if (empty($_POST['user']) || empty($_POST['password']))
-{
-	echo 'Please fill in your Username and Password<br /><br />';
-	echo 'Click <a href="./index.php">here</a> to come back to login page';
-}
-else
-{
-	// Check received data length (to prevent code injection)
-	if (strlen($_POST['user']) > 15)
- 	{
-  		echo 'Username has incorrect format ... Please try again<br /><br />';
-		echo 'Click <a href="./index.php">here</a> to come back to login page';
+if (empty($_POST['user']) || empty($_POST['password'])) {
+    echo 'Please fill in your Username and Password<br /><br />';
+    echo 'Click <a href="./index.php">here</a> to come back to login page';
+} else {
+    // Check received data length (to prevent code injection)
+    if (strlen($_POST['user']) > 15) {
+        echo 'Username has incorrect format ... Please try again<br /><br />';
+        echo 'Click <a href="./index.php">here</a> to come back to login page';
+    } elseif (strlen($_POST['password']) > 50 || strlen($_POST['password']) <= 2) {
+        echo 'Password has incorrect format ... Please try again<br /><br />';
+        echo 'Click <a href="./index.php">here</a> to come back to login page';
+    } else {
+        // Remove every html tag and useless space on username (to prevent XSS)
+        $user = strip_tags(trim($_POST['user']));
+
+        $user = $_POST['user'];
+        $password = $_POST['password'];
+
+        // Open a LDAP connection
+        $ldap = new LDAP($ldap_host, $ldap_port, $ldap_version);
+
+        // Check user credential on LDAP
+        try {
+            foreach ($ldap_base_dn as $ldap_base_dn_entry) {
+                try {
+                    $authenticated = $ldap->checkLogin($user, $password, $ldap_search_attribute, $ldap_filter, $ldap_base_dn_entry, $ldap_bind_dn, $ldap_bind_pass);
+                } catch (Exception $exception){
+                    continue;
+                }
+                break;
+            }
+            if(!$authenticated){
+                throw new Exception('An error occured while trying to check user login.');
+            }
+        } catch (Exception $e) {
+            $resp = json_encode(array("error" => "Impossible to get data", "message" => $e->getMessage()));
+            $authenticated = false;
+        }
+
+        // If user is authenticated
+        if ($authenticated) {
+            $_SESSION['uid'] = $user;
+
+            // If user came here with an autorize request, redirect him to the authorize page. Else prompt a simple message.
+            if (isset($_SESSION['auth_page'])) {
+                $auth_page = $_SESSION['auth_page'];
+                header('Location: ' . $auth_page);
+                exit();
+            } else {
+                echo "Congratulation you are authenticated ! <br /><br /> However there is nothing to do here ...";
+            }
+        } // check login on LDAP has failed. Login and password were invalid or LDAP is unreachable
+        else {
+            echo "Authentication failed ... Check your username and password.<br />If error persist contact your administrator.<br /><br />";
+            echo 'Click <a href="./index.php">here</a> to come back to login page';
+            echo '<br /><br /><br />' . $resp;
+        }
     }
-    elseif (strlen($_POST['password']) > 50 || strlen($_POST['password']) <= 2)
-    {
-    	echo 'Password has incorrect format ... Please try again<br /><br />';
-		echo 'Click <a href="./index.php">here</a> to come back to login page';
-    }
-    else
-   	{
-   		// Remove every html tag and useless space on username (to prevent XSS)
-   	   	$user=strip_tags(trim($_POST['user']));
-
-    	$user=$_POST['user'];
-    	$password=$_POST['password'];
-
-    	// Open a LDAP connection
-    	$ldap = new LDAP($ldap_host,$ldap_port,$ldap_version);
-
-		// Check user credential on LDAP
-		try{
-			$authenticated = $ldap->checkLogin($user,$password,$ldap_search_attribute,$ldap_filter,$ldap_base_dn,$ldap_bind_dn,$ldap_bind_pass);
-		}
-		catch (Exception $e)
-		{
-			$resp = json_encode(array("error" => "Impossible to get data", "message" => $e->getMessage()));
-			$authenticated = false;
-		}
-
-		// If user is authenticated
-		if ($authenticated)
-		{
-		    $_SESSION['uid']=$user;
-
-		    // If user came here with an autorize request, redirect him to the authorize page. Else prompt a simple message.
-		    if (isset($_SESSION['auth_page']))
-		    {
-		    	$auth_page=$_SESSION['auth_page'];
-		    	header('Location: ' . $auth_page);
- 				exit();
-		    }
-		 	else
-		 	{
-		 		echo "Congratulation you are authenticated ! <br /><br /> However there is nothing to do here ...";
-			}
-	    }
-	    // check login on LDAP has failed. Login and password were invalid or LDAP is unreachable
-		else
-		{
-		echo "Authentication failed ... Check your username and password.<br />If error persist contact your administrator.<br /><br />";
-		echo 'Click <a href="./index.php">here</a> to come back to login page';
-		echo '<br /><br /><br />' . $resp;
-		}
-	}
 }
--- oauth/resource.php
+++ oauth/resource.php
@@ -2,15 +2,15 @@
 /**
  * @author Denis CLAVIER <clavierd at gmail dot com>
  * Adapted from Oauth2-server-php cookbook
- * @see http://bshaffer.github.io/oauth2-server-php-docs/cookbook/
+ * @see    http://bshaffer.github.io/oauth2-server-php-docs/cookbook/
  */
 
 // include our OAuth2 Server object
-require_once __DIR__.'/server.php';
+require_once __DIR__ . '/server.php';
 
 // include our LDAP object
-require_once __DIR__.'/LDAP/LDAP.php';
-require_once __DIR__.'/LDAP/config_ldap.php';
+require_once __DIR__ . '/LDAP/LDAP.php';
+require_once __DIR__ . '/LDAP/config_ldap.php';
 
 // Handle a request to a resource and authenticate the access token
 if (!$server->verifyResourceRequest(OAuth2\Request::createFromGlobals())) {
@@ -27,22 +27,29 @@ $user = $info_oauth["user_id"];
 $assoc_id = intval($info_oauth["assoc_id"]);
 
 // Open a LDAP connection
-$ldap = new LDAP($ldap_host,$ldap_port,$ldap_version);
+$ldap = new LDAP($ldap_host, $ldap_port, $ldap_version);
 
 // Try to get user data on the LDAP
-try
-{
-	$data = $ldap->getDataForMattermost($ldap_base_dn,$ldap_filter,$ldap_bind_dn,$ldap_bind_pass,$ldap_search_attribute,$user);
+try {
+    foreach ($ldap_base_dn as $ldap_base_dn_entry) {
+        try {
+            $data = $ldap->getDataForMattermost($ldap_base_dn_entry, $ldap_filter, $ldap_bind_dn, $ldap_bind_pass, $ldap_search_attribute, $user);
+        } catch (Exception $exception) {
+            continue;
+        }
+        break;
+    }
+    if (!$data) {
+        throw new Exception('An error occured while fetching user data from LDAP.');
+    }
 
-	// Here is the patch for Mattermost 4.4 and older. Gitlab has changed the JSON output of oauth service. Many data are not used by Mattermost, but there is a stack error if we delete them. That's the reason why date and many parameters are null or empty.
-	//$resp = array("id" => $assoc_id,"name" => $data['cn'],"username" => $user,"state" => "active","avatar_url" => "","web_url" => "","created_at" => "0000-00-00T00:00:00.000Z","bio" => null,"location" => null,"skype" => "","linkedin" => "","twitter" => "","website_url" => "","organization" => null,"last_sign_in_at" => "0000-00-00T00:00:00.000Z","confirmed_at" => "0000-00-00T00:00:00.000Z","last_activity_on" => null,"email" => $data['mail'],"theme_id" => 1,"color_scheme_id" => 1,"projects_limit" => 100000,"current_sign_in_at" => "0000-00-00T00:00:00.000Z","identities" => array(array("provider" => "ldapmain","extern_uid" => $data['dn'])),"can_create_group" => true,"can_create_project" => true,"two_factor_enabled" => false,"external" => false,"shared_runners_minutes_limit" => null);
+    // Here is the patch for Mattermost 4.4 and older. Gitlab has changed the JSON output of oauth service. Many data are not used by Mattermost, but there is a stack error if we delete them. That's the reason why date and many parameters are null or empty.
+    //$resp = array("id" => $assoc_id,"name" => $data['cn'],"username" => $user,"state" => "active","avatar_url" => "","web_url" => "","created_at" => "0000-00-00T00:00:00.000Z","bio" => null,"location" => null,"skype" => "","linkedin" => "","twitter" => "","website_url" => "","organization" => null,"last_sign_in_at" => "0000-00-00T00:00:00.000Z","confirmed_at" => "0000-00-00T00:00:00.000Z","last_activity_on" => null,"email" => $data['mail'],"theme_id" => 1,"color_scheme_id" => 1,"projects_limit" => 100000,"current_sign_in_at" => "0000-00-00T00:00:00.000Z","identities" => array(array("provider" => "ldapmain","extern_uid" => $data['dn'])),"can_create_group" => true,"can_create_project" => true,"two_factor_enabled" => false,"external" => false,"shared_runners_minutes_limit" => null);
 
-	// Below is the old version, still consistent with Mattermost before version 4.4
-	$resp = array("name" => $data['cn'],"username" => $user,"id" => $assoc_id,"state" => "active","email" => $data['mail']);
-}
-catch (Exception $e)
-{
-	$resp = array("error" => "Impossible to get data", "message" => $e->getMessage());
+    // Below is the old version, still consistent with Mattermost before version 4.4
+    $resp = array("name" => $data['cn'], "username" => $user, "id" => $assoc_id, "state" => "active", "email" => $data['mail']);
+} catch (Exception $e) {
+    $resp = array("error" => "Impossible to get data", "message" => $e->getMessage());
 }
 
 // send data or error message in JSON format
